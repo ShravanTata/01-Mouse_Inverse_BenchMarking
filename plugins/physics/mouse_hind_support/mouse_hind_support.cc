6e5d827452dfa1ab7096b1a36c619ae66728f72c
tatarama <tatarama@tsf-444-wpa-4-206.epfl.ch>
2017-05-29 17:42:52 +0200

    Fixed physics plugins : Removed temporary ones

--- plugins/physics/mouse_hind_support/mouse_hind_support.cc
+++ plugins/physics/mouse_hind_support/mouse_hind_support.cc
@@ -9,7 +9,7 @@
 
 #include <ode/ode.h>
 #include <plugins/physics.h>
-#include <string>
+
 
 static dBodyID body_Spine;
 static dBodyID body_Reference;
@@ -19,25 +19,7 @@ static dJointID joint_Fixed;
 static dJointID joint_Slider_Y;
 static dJointID joint_Slider_Z;
 
-static const dReal* body_Spine_pos; //position (X,Y,Z) of mouse
-static const dReal* body_Spine_vel; //linear velocity
-static dReal lift_force;
-
 void webots_physics_step() {
-
-	// Get Position and Velocity of Spine
-	
-	body_Spine_pos = dBodyGetPosition (body_Spine);
-	body_Spine_vel = dBodyGetLinearVel(body_Spine);
-	
-	dWebotsConsolePrintf("Position %f",body_Spine_pos[1]);
-
-	lift_force = 75000 * (- 0.5 - body_Spine_pos[1]) - 1000*body_Spine_vel[1];
-
-	dBodyAddForce(body_Spine, 0, lift_force ,0);
-	//
-	//dBodySetPosition (body_Spine, 0, 0, 4);
-
 
 }
 
@@ -49,7 +31,7 @@ void webots_physics_init()
 	body_Spine = dWebotsGetBodyFromDEF("spine");
 	if (body_Spine == NULL)
 	{	//we check we found the body
-		dWebotsConsolePrintf("ERROR: could not find the body_Spine \n In physic plug-in.\n");
+		dWebotsConsolePrintf("ERROR: could not find the body_Spine \n    In physic plug-in.\n");
 		return;
 	}
 
@@ -59,11 +41,11 @@ void webots_physics_init()
 
 	// Create a fixed body attached to the world
 
-	joint_Fixed = dJointCreateFixed(world, 0); // Create a fixed joint with the world
+	// joint_Fixed = dJointCreateFixed(world, 0); // Create a fixed joint with the world
 
-	dJointAttach (joint_Fixed, body_Reference, 0); // Fix the reference body to the world
+	// dJointAttach (joint_Fixed, body_Reference, 0); // Fix the reference body to the world
 
-	dJointSetFixed (joint_Fixed); // Initialize the joint
+	// dJointSetFixed (joint_Fixed); // Initialize the joint
 
 
 	// Create a sliding joint in Z direction for lateral movement support between the reference body and the spine
@@ -74,7 +56,7 @@ void webots_physics_init()
 
 	dJointSetSliderAxis(joint_Slider_Z, 0 ,0, 1); // Create a slider along the z-axis
 
-	// Create a prismatic-rotaide joint in Y direction for height support between the reference and the spine
+	// Create a sliding joint in Y direction for height support between the reference and the spine
 
 	joint_Slider_Y = dJointCreateSlider(world,0); // Create a slider joint
 
@@ -82,7 +64,6 @@ void webots_physics_init()
 
 	dJointSetSliderAxis(joint_Slider_Y, 0 ,1, 0); // Create a slider along the z-axis
 
-	//dJointSetSliderParam (joint_Slider_Y, dParamLoStop  , -0.1);
 
 }
 
